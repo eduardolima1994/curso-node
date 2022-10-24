3ebdacd1b113c61c1562d0ad2cc09c11a6e9d7e2
Eduardo Lima <eduardolima.tecnico@gmail.com>
2022-10-24 16:36:50 -0300

    feat: added get and post

--- node-basic/src/index.js
+++ node-basic/src/index.js
@@ -1,25 +1,23 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 const app = express()
-
 app.use(express.json())
 
+const projects = []
+
 app.get('/projects', (request, response)=>{
-  const { name, age } = request.query
-  console.log(name, age)
-  return response.json([
-    'Projeto 1',
-    'Projeto 2'
-  ])
+  return response.json(projects)
 })
 
 app.post('/projects', (request, response)=>{
-  const { name, age } = request.body
-  console.log(name, age)
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3'
-  ])
+  const { name, owner } = request.body
+  const project = {
+    id: uuidv4(),
+    name,
+    owner
+  }
+  projects.push(project)
+  return response.status(201).json(project)
 })
 
 app.put('/projects/:id', (request, response)=>{
